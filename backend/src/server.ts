03a4f7758add1ca5056df6ca2a00bba15848270b
Marcos Domingues <mdomingues.ti@gmail.com>
2020-10-15 17:53:34 -0300

    * NLW - Workshop #02 - * NLW - Workshop #02 - Controller : Listas, Criar, Detalhar ...

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,12 +1,10 @@
 import express from 'express';
-
 import './database/connection';
+import routes from './routes';
 
 const app = express();
-app.use(express.json()); // -> Habilita o "body" do request
 
-app.get('/users', (request, response) => {
-  return response.json({ message: 'Hello word'});
-});
+app.use(express.json()); // -> Habilita o "body" do request
+app.use(routes);
 
 app.listen(3333);
