3f1728bffd179971f87f323fd0240e9f5fd82569
Marcos Domingues <mdomingues.ti@gmail.com>
2020-10-16 13:34:49 -0300

    NLW - Workshop #02 - Validations

--- backend/src/controllers/OrphanagesController.ts
+++ backend/src/controllers/OrphanagesController.ts
@@ -2,6 +2,7 @@ import { Request, Response } from 'express';
 import { getRepository } from 'typeorm';
 import Orphanage from '../models/Orphanage';
 import orphanageView from '../views/orphanages_view';
+import * as Yup from 'yup';
 
 export default {
 
@@ -45,12 +46,12 @@ export default {
     const orphanagesRepository = getRepository(Orphanage);
 
     const requestImages = request.files as Express.Multer.File[];
-    
+
     const images = requestImages.map(image => {
       return { path: image.filename }
     })
 
-    const orphanage = orphanagesRepository.create({
+    const data = {
       name,
       latitude,
       longitude,
@@ -59,7 +60,28 @@ export default {
       opening_hours,
       open_on_weekends,
       images
+    }
+
+    // > Validation object
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Nome obrigatório'),
+      latitude: Yup.number().required('Latitude obrigatória'),
+      about: Yup.string().required('Sobre obrigatório').max(300, 'Sobre: no máximo 300 caracteres'),
+      instructions: Yup.string().required('Instruções são obrigatórias'),
+      opening_hours: Yup.string().required('Horário obrigatório'),
+      open_on_weekends: Yup.string().required('Aberto aos fins de semana obrigatório'),
+      images: Yup.array(
+        Yup.object().shape({
+          path: Yup.string().required()
+        })
+      )
     });
+
+    await schema.validate(data, {
+      abortEarly: false, // retornar todos erros ao mesmo tempo
+    })
+
+    const orphanage = orphanagesRepository.create(data);
 
     await orphanagesRepository.save(orphanage);
 
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,5 +1,12 @@
-import path from 'path';
 import express from 'express';
+import path from 'path';
+
+//-> Bloqueio de outros domínios
+//    Faça o import do 'cors' para desabilitar a função de 
+//    segurança do Node.js, que faz bloqueio 
+//    que impede requisições de outros domínios.
+import cors from 'cors'; 
+
 import 'express-async-errors';
 
 import './database/connection';
@@ -20,6 +27,9 @@ app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')))
 
 // -> Tratamento de exceções
 app.use(errorHandler);
+
+// -> Habilitar outros domínios
+app.use(cors());
 
 // -> Configura porta
 app.listen(3333);
