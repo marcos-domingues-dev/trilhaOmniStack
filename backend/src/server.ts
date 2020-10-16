7bb802042a847ff51bb0a8c3bb9f457b2fa9756b
Marcos Domingues <mdomingues.ti@gmail.com>
2020-10-16 11:38:22 -0300

    NLW - Workshop #02 - Exceptions

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,11 @@
 import path from 'path';
 import express from 'express';
+import 'express-async-errors';
 
 import './database/connection';
 
 import routes from './routes';
+import errorHandler from './errors/handler'
 
 const app = express();
 
@@ -15,6 +17,9 @@ app.use(routes);
 
 // -> Rota estática: libera acesso ao diretório ""../uploads"
 app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads'))) 
+
+// -> Tratamento de exceções
+app.use(errorHandler);
 
 // -> Configura porta
 app.listen(3333);
